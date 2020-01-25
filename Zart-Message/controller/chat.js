14801a360782c556434b2af4bcb50904fc9281f2
Meisam Poorzand <mpoorzand@avtr.com>
2020-01-24 17:51:29 -0800

    adds all chats method to chat chontroller

--- Zart-Message/controller/chat.js
+++ Zart-Message/controller/chat.js
@@ -35,20 +35,20 @@ module.exports = {
 
   function allChats(req , res){
     let chatNameAndId = []
-    // User.findById(req.body._id,  function(err , user){
-    //   if(user.chats.length != null){
-    //    user.chats.forEach( chatId =>{
-    //      Chat.findById(chatId ,  function(err , chat){
-    //       let result
-    //        chat.users.indexOf(user._id) ? result = chat.users[0] : result = chat.users[1]
-    //        User.findById(result ,  function(err , reciver){
-    //          chatNameAndId.push({[reciver.name] : chatId})
-    //         if(user.chats.length == chatNameAndId.length) return res.json(chatNameAndId)
-    //       })
-    //     })
-    //   })
-    // }
-    // });
+    User.findById(req.body._id,  function(err , user){
+      if(user.chats.length != null){
+       user.chats.forEach( chatId =>{
+         Chat.findById(chatId ,  function(err , chat){
+          let result
+           chat.users.indexOf(user._id) ? result = chat.users[0] : result = chat.users[1]
+           User.findById(result ,  function(err , reciver){
+             chatNameAndId.push({[reciver.name] : chatId})
+            if(user.chats.length == chatNameAndId.length) return res.json(chatNameAndId)
+          })
+        })
+      })
+    }
+    });
   }
 
 function chatSelected(req,res){
